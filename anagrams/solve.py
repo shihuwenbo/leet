e0efa22523514dd652fca3ddc7bae191516ebfd9
Huwenbo Shi <shihuwenboinus@gmail.com>
2015-03-15 21:40:05 -0700

    added files

---
+++ anagrams/solve.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param strs, a list of strings
+    # @return a list of strings
+    def anagrams(self, strs):
+        print strs
+        
+
+sol = Solution()
+print sol.anagrams(['hello', 'olleh', 'haha', 'ahha'])
