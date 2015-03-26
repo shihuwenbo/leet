722710f067437468e3efa5233d2942f9abbb8149
Huwenbo Shi <shihuwenboinus@gmail.com>
2015-03-25 20:38:43 -0700

    added several problems

---
+++ pascal_triangle/src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new List<List<Integer> >();
+    	return result;
+    }
+    
+    public static void main(String[] args) {
+    	Solution sol = new Solution();
+    	
+    }
+}
