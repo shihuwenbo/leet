13f56ac91a4801899f1c0a7d48d7ed38e24c8480
shihuwenbo <shihuwenboinus@gmail.com>
2015-08-04 20:05:36 -0700

    added new solutions

--- max_prod_subarray/solve.cpp
+++ max_prod_subarray/solve.cpp
@@ -5,30 +5,26 @@ using namespace std;
 
 class Solution {
 public:
+    int max3(int a, int b, int c) {
+        int tmp = a > b ? a : b;
+        return tmp > c ? tmp : c;
+    }
+
+    int min3(int a, int b, int c) {
+        return -max3(-a, -b, -c);
+    }
+
     int maxProduct(vector<int>& nums) {
-        
-        if(nums.size() <= 1) {
-            return nums[0];
+        int max_prod = nums[0];
+        int min_prod = nums[0];
+        int ret_val = nums[0];
+        for(int i=1; i<nums.size(); i++) {
+            int max_prod_new = max3(nums[i], nums[i]*max_prod, nums[i]*min_prod);
+            int min_prod_new = min3(nums[i], nums[i]*min_prod, nums[i]*max_prod);
+            max_prod = max_prod_new;
+            min_prod = min_prod_new;
+            ret_val = ret_val > max_prod ? ret_val : max_prod;
         }
-
-        int max_keep = nums[1]*nums[0];
-        int max_skip = nums[0];
-
-        for(int i=2; i<nums.size(); i++) {
-
-            cout << max_keep << " " << max_skip << endl;
-
-            int tmp = nums[i]*max_keep;
-            int max_keep_new = nums[i]>tmp ? nums[i] : tmp;
-            int max_skip_new = max_keep>max_skip ? max_keep : max_skip;
-            
-            max_keep = max_keep_new;
-            max_skip = max_skip_new;
-        }
-
-        cout << max_keep << " " << max_skip << endl;
-
-        int ret_val = max_keep>max_skip ? max_keep : max_skip;
 
         return ret_val;
     }
@@ -36,9 +32,10 @@ public:
 
 int main() {
     vector<int> nums;
-    nums.push_back(-1);
-    nums.push_back(-1);
+    nums.push_back(2);
+    nums.push_back(3);
     nums.push_back(-2);
+    nums.push_back(4);
     Solution sol;
     cout << sol.maxProduct(nums) << endl;
 }
