9eae01aa642be0b02bf2caf089945330d8c8e9cc
shihuwenbo <shihuwenboinus@gmail.com>
2015-08-23 09:26:56 +0800

    added solution to jump game

--- jump_game/solution.cpp
+++ jump_game/solution.cpp
@@ -2,29 +2,14 @@
 #include <stdio.h>
 
 bool canJump(int* nums, int numsSize) {
-    
-    int idx = 0;
-    while(true) {
-        if(idx >= numsSize-1) {
-            return true;
-        }
-        int new_idx = idx;
-        int num_jump = nums[idx+1];
-        for(int i=1; i<=nums[idx] && idx+i<numsSize; i++) {
-            if(idx+i == numsSize-1) {
-                return true;
-            }
-            if(nums[idx+i] >= num_jump) {
-                new_idx = idx+i;
-            }
-        }
-        idx = new_idx;
-        if(idx < numsSize-1 && nums[idx] == 0) {
+    int i, step = nums[0];
+    for(i=1; i<numsSize; i++) {
+        step--;
+        if(step < 0)
             return false;
-        }
+        step = nums[i]>step ? nums[i] : step;
     }
-
-    return false;
+    return true;
 }
 
 int main() {
