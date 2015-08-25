35cc403d0091b9beacced9c4c152a627057c2cb7
shihuwenbo <shihuwenboinus@gmail.com>
2015-08-25 21:40:16 +0800

    added word break and flatten bst

--- word_break/tmp_solution.cpp
+++ word_break/tmp_solution.cpp
@@ -2,55 +2,43 @@
 #include <unordered_set>
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    bool helper(string s, unordered_set<string>& wordDict, 
-                unordered_map<string, bool>& record) {
-        if(s == "")
-            return false;
+    unordered_map<string, bool> record;
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
         if(wordDict.find(s) != wordDict.end())
             return true;
-        for(int i=0; i<s.size(); i++) {
-            string left_half = s.substr(0,i);
-            string right_half = s.substr(i,s.size()-i);
-            bool left_ok = false, right_ok = false;
-            if(record.find(left_half) != record.end()) {
+        for(int i=0; i<s.size()-1; i++) {
+            string left_half = s.substr(0, i+1);
+            string right_half = s.substr(i+1, s.size()-i-1);
+            bool left_ok, right_ok;
+            if(record.find(left_half) != record.end())
                 left_ok = record[left_half];
-            }
             else {
-                left_ok = helper(left_half, wordDict, record);
+                left_ok = wordBreak(left_half, wordDict);
                 record[left_half] = left_ok;
             }
-            if(!left_ok)
-                continue;
-            if(record.find(right_half) != record.end()) {
+            if(record.find(right_half) != record.end())
                 right_ok = record[right_half];
-            }
             else {
-                right_ok = helper(right_half, wordDict, record);
+                right_ok = wordBreak(right_half, wordDict);
                 record[right_half] = right_ok;
             }
-            if(!right_ok)
-                continue;
-            if(left_ok && right_ok) {
+            if(left_ok && right_ok)
                 return true;
-            }
         }
         return false;
-    }
-
-    bool wordBreak(string s, unordered_set<string>& wordDict) {
-        unordered_map<string, bool> record;
-        return helper(s, wordDict, record);
     }
 };
 
 int main() {
     unordered_set<string> wdict;
-    wdict.insert("a");
+    wdict.insert("leet");
+    wdict.insert("code");
     Solution sol;
-    cout << sol.wordBreak("a", wdict) << endl;
+    cout << sol.wordBreak("leetcode", wdict) << endl;
 }
